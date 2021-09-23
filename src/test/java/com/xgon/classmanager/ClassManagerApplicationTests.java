9f6ad596d6df13574518b4bda262b89203e31068
xgon <laoshi1998@outlook.com>
2021-09-23 16:22:35 +0800

    test: first

---
+++ src/test/java/com/xgon/classmanager/ClassManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgon.classmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
