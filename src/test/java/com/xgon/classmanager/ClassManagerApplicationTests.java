b2333320a05f19dda955275233952cf20978d0a7
xgon <laoshi1998@outlook.com>
2021-09-27 09:47:57 +0800

    fix: all

--- src/test/java/com/xgon/classmanager/ClassManagerApplicationTests.java
+++ src/test/java/com/xgon/classmanager/ClassManagerApplicationTests.java
@@ -8,6 +8,8 @@ class ClassManagerApplicationTests {
 
     @Test
     void contextLoads() {
+        // System.getProperty("user.dir") 表示当前项目所在的目录（src目录的父目录的绝对路径）
+        System.out.println(System.getProperty("user.dir"));
     }
 
 }
